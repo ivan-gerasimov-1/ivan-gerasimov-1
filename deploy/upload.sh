51a258bddddc95bf8f2736a20e25e5c9a7472c7f
EvanJ0hnson <ivangerasimow@gmail.com>
2021-08-15 17:57:12 +0300

    Fix deployment scripts and pipelines

--- deploy/upload.sh
+++ deploy/upload.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd dist && rsync -e "ssh -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no" --archive --compress --delete . root@8156138.gq:/srv/gerasimov.pw
+cd ../
 cd src/public && rsync -e "ssh -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no" --archive --compress . root@8156138.gq:/srv/gerasimov.pw
