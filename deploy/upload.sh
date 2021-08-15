e102313a65b8858509da966c1b2fa867bda70394
EvanJ0hnson <ivangerasimow@gmail.com>
2021-08-15 17:53:15 +0300

    Update path in upload.sh

--- deploy/upload.sh
+++ deploy/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd /dist && rsync -e "ssh -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no" --archive --compress --delete . root@8156138.gq:/srv/gerasimov.pw
-cd /src/public && rsync -e "ssh -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no" --archive --compress . root@8156138.gq:/srv/gerasimov.pw
+cd dist && rsync -e "ssh -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no" --archive --compress --delete . root@8156138.gq:/srv/gerasimov.pw
+cd src/public && rsync -e "ssh -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no" --archive --compress . root@8156138.gq:/srv/gerasimov.pw
