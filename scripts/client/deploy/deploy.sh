6119d504439ee9619aab5aaeda74ad01fded7ccd
EvanJ0hnson <ivangerasimow@gmail.com>
2021-08-28 14:37:59 +0300

    Move client scripts to scripts/client

---
+++ scripts/client/deploy/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export DEPLOY_SRC=.tmp/deploy
+export SCRIPTS_DIR = scripts/client
+
+sh $SCRIPTS_DIR/deploy/pre-upload.sh
+sh $SCRIPTS_DIR/deploy/upload.sh
--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-export DEPLOY_SRC=.tmp/deploy
-
-sh scripts/deploy/pre-upload.sh
-sh scripts/deploy/upload.sh
+sh scripts/client/deploy/deploy.sh
