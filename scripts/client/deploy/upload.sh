f037a144c3f3007b717e9fec73ebf616789021e7
Ivan Gerasimov <ivangerasimow@gmail.com>
2024-04-16 17:09:29 +0000

    Update client deploy

--- scripts/client/deploy/upload.sh
+++ scripts/client/deploy/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo $LOG_PREFIX Upload app to the server
-rsync -e "ssh -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no" --archive --compress --delete $DEPLOY_SRC/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DEST
+rsync -e "ssh -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no" --archive --compress --delete $DEPLOY_SRC/* $DEPLOY_USER@$DEPLOY_HOST:~/srv/html
 echo $LOG_PREFIX ✓ Done
