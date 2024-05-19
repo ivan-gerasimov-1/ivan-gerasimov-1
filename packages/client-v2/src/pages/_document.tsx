69c8b044d55835fada8da45a6c994191c69b56d9
Ivan Gerasimov <ivangerasimow@gmail.com>
2024-05-19 13:35:46 +0700

    Move metadata from document to app

--- packages/client-v2/src/pages/_document.tsx
+++ packages/client-v2/src/pages/_document.tsx
@@ -3,17 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export function Document() {
 	return (
 		<Html lang="en">
-			<Head>
-				<title>Ivan Gerasimov</title>
-				<meta
-					name="description"
-					content="Ivan Gerasimov (Иван Герасимов): Fullstack JavaScript developer"
-				/>
-				<meta
-					name="keywords"
-					content="frontend, front-end, front end, developer, upwork, freelance, javascript, js, react, reactjs, vue, vuejs, vue.js, node, node.js, nodejs, angular, angularjs, angular.js, cоздание, сайт, сайтов, веб, разработка, веб-разработка"
-				/>
-			</Head>
+			<Head />
 
 			<body>
 				<Main />
