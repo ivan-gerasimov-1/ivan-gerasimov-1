b1731a7eeeb9bae601b64201184d895f19f3bd28
Ivan Gerasimov <ivangerasimow@gmail.com>
2025-02-17 20:18:23 +0400

    Move Playground’s meta to i18n

--- apps/client/src/i18n/playground/en/playground.ts
+++ apps/client/src/i18n/playground/en/playground.ts
@@ -1,6 +1,8 @@
+import type { TPageMeta } from "#app/i18n/types.ts";
+
 import { keywords } from "#app/i18n/keywords/en/keywords.ts";
 
-export let meta = {
+export const meta: TPageMeta = {
 	keywords: keywords.join(", "),
 
 	description: "Ivan Gerasimov | Software Engineer (JavaScript, Fullstack)",
